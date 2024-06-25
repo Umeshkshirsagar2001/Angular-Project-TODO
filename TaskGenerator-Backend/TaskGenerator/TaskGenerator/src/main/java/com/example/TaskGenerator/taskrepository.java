27d9485e035d21cdc198f6cc48b7bddf138693ef
umesh kshirsagar <89686640+Umeshkshirsagar2001@users.noreply.github.com>
2024-06-25 13:47:13 +0530

    Delete TaskGenerator-Backend/TaskGenerator directory

--- TaskGenerator-Backend/TaskGenerator/TaskGenerator/src/main/java/com/example/TaskGenerator/taskrepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.TaskGenerator;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface taskrepository extends JpaRepository<Task, Long> {
-    
-}
--- TaskGenerator-Backend/TaskGenerator/src/main/java/com/todo/taskgenerator/taskrepository.java
+++
@@ -1,7 +0,0 @@
-package com.todo.taskgenerator;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface taskrepository extends JpaRepository<Task, Long> {
-    
-}
